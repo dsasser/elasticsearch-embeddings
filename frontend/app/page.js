a4a4241010a6cd1be4378d6293272a9f99476dff
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-09 14:08:11 -0700

    Initial page.

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,13 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
-        <h1>Search with Elasticsearch Embeddings</h1>
-        <label hidden={true} htmlFor="search">Search</label>
-        <input className="border-2 border-gray-300 rounded-md p-2" type="text" placeholder="Search" />
-        <button className="bg-blue-500 text-white p-2 rounded-md">Search</button>
+        <div className="flex items-center justify-center h-screen">
+        <div className="text-center">
+          <h1 className="mb-4">Search with Elasticsearch Embeddings</h1>
+          <label hidden={true} htmlFor="search">Search</label>
+          <input className="mr-1 border-2 border-gray-300 rounded-md p-2" type="text" placeholder="Search" />
+          <button className="bg-blue-500 text-white p-2 rounded-md">Search</button>
+          </div>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p>

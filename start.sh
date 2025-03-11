a8a8d6969e466b1978845d936267be4a551b6c07
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-10 19:05:02 -0700

    Removing mutagen for now.

--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# # Start Mutagen sync
-# echo "Starting Mutagen sync..."
-# mutagen sync start
-
-# # Check if Mutagen sync started successfully
-# if [ $? -ne 0 ]; then
-#   echo "Error: Failed to start Mutagen sync."
-#   exit 1
-# fi
-
 # Start Docker Compose services
 echo "Starting Docker Compose services..."
 docker-compose up --build -d
@@ -20,10 +10,4 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo "Starting Next.js development server..."
-cd frontend
-npm install
-npm run build
-npm run dev
-
-# echo "Both Mutagen and Docker Compose services are running."
+echo "Docker Compose services are running."

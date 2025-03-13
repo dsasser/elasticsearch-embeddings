ed9ca90bd0bd78c22d17bfa26e860749a8d75fb6
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-13 11:48:49 -0700

    Moving to separate docker compose files.

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Start Docker Compose services
-echo "Starting Docker Compose services..."
-docker-compose up --build -d
 
-# Check if Docker Compose started successfully
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to start Docker Compose services."
-  exit 1
-fi
+# Start Elasticsearch and friends.
+echo "Starting Elasticsearch..."
+docker-compose -f backend/elasticsearch/docker-compose.yml up -d
 
-echo "Docker Compose services are running."
+# Start the crawler.
+echo "Starting crawler..."
+docker-compose -f backend/crawler/docker-compose.yml up -d
+
+
+# Start the frontend.
+echo "Starting frontend..."
+docker-compose -f frontend/docker-compose.yml up -d
+
+
+

4ae20c64777a6d18eecacddb962eeaca34ff9bd4
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-22 11:19:35 -0700

    Use form submission to capture 'enter' as a submit trigger. Just makes it easier to search.

--- frontend/app/components/SearchInput.js
+++ frontend/app/components/SearchInput.js
@@ -9,7 +9,8 @@ export default function SearchInput({query, onSearch}) {
     setInput(query);
   }, [query]);
 
-  const handleClick = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (!input.trim()) return;
     onSearch(input);
   };
@@ -17,24 +18,26 @@ export default function SearchInput({query, onSearch}) {
   return (
     <div className="search-input-container flex flex-row items-start justify-center">
       <div className="search-input">
-        <label hidden={true} htmlFor="search">Search</label>
-        <input 
-          id="search"
-          value={input} 
-          onChange={(e) => setInput(e.target.value)} 
-          className="mr-1 border-2 border-gray-300 rounded-md p-2" 
-          type="text" 
-          placeholder="Search" 
-        />
-        <label hidden={true} htmlFor="search-button">Search with Elastic full text search and OpenAI embeddings</label>
-        <button 
-          type="submit"
-          id="search-button"
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition"
-          onClick={handleClick}
-        >
-          Search
-        </button>
+        <form onSubmit={handleSubmit}>
+          <label hidden={true} htmlFor="search">Search</label>
+          <input 
+            id="search"
+            value={input} 
+            onChange={(e) => setInput(e.target.value)} 
+            className="mr-1 border-2 border-gray-300 rounded-md p-2" 
+            type="text" 
+            placeholder="Search" 
+          />
+          <label hidden={true} htmlFor="search-button">Search with Elastic full text search and OpenAI embeddings</label>
+          <button 
+            type="submit"
+            id="search-button"
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition"
+          >
+            Search
+          </button>
+        </form>
+        
       </div>
     </div>
   )

6e51d773675cc8e71c82cde629ffa1405da5b199
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-23 19:17:14 -0700

    Update copy-certs.sh to respect compose namespace.

--- scripts/copy-certs.sh
+++ scripts/copy-certs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker cp elasticsearch-embeddings-es01-1:/usr/share/elasticsearch/config/certs ./certs
+set -a
+source .env
+set +a
+
+docker cp "${COMPOSE_PROJECT_NAME}"-es01-1:/usr/share/elasticsearch/config/certs ./certs

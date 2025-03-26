56b0176f8d523669d6ec6796f648965b67f990ad
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-25 20:30:14 -0700

    Allow stop.sh to take arguments. This allows for a 'nuke' easily.

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,12 +2,12 @@
 
 # Stopping Elasticsearch and Kibana..
 echo "Stopping Elasticsearch..."
-docker-compose --env-file .env -f backend/elasticsearch/docker-compose.yml down
+docker-compose --env-file .env -f backend/elasticsearch/docker-compose.yml down "$@"
 
 # Stopping the crawler.
 echo "Stopping crawler..."
-docker-compose --env-file .env -f backend/crawler/docker-compose.yml down
+docker-compose --env-file .env -f backend/crawler/docker-compose.yml down "$@"
 
 # Stopping NextJS.
 echo "Stopping NextJS..."
-docker-compose --env-file .env -f frontend/docker-compose.yml down
+docker-compose --env-file .env -f frontend/docker-compose.yml down "$@"

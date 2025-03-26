80ef461b83054a6e0c0e33541a07cb210d00eb0c
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-25 18:03:37 -0700

    Update copy-certs script to only copy the public keys we need.

--- scripts/copy-certs.sh
+++ scripts/copy-certs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker cp elasticsearch:/usr/share/elasticsearch/config/certs ./certs
+docker cp elasticsearch:/usr/share/elasticsearch/config/certs/es01/es01.crt ./certs/es01.crt
+docker cp elasticsearch:/usr/share/elasticsearch/config/certs/ca/ca.crt ./certs/ca.crt
